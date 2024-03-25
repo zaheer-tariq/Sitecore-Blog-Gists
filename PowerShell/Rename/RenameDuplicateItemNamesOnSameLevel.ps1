cec9064c4fe4d5d64bf5f75e6d7c4ba448e4cd67
Zaheer Tariq <mzaheertariq@gmail.com>
2024-03-26 03:41:20 +0500

    Update RenameDuplicateItemNamesOnSameLevel.ps1

--- PowerShell/Rename/RenameDuplicateItemNamesOnSameLevel.ps1
+++ PowerShell/Rename/RenameDuplicateItemNamesOnSameLevel.ps1
@@ -1,11 +1,11 @@
 Get-ChildItem -Path "master:/sitecore/content/Settings/Products" -Recurse | Foreach-Object{
 	$id = $_.ID
 	$item = Get-Item -Path master: -ID $id
-	Write-Output "Check item $($item.Name)"
+	Write-Output "Checking item $($item.Name)"
 	$index = 1
 	Get-ChildItem -Path $item.Parent.Paths.FullPath | Where-Object{ $_.ID.Guid -ne $id.Guid -and $_.Name -eq $item.Name } | Foreach-Object {
 		$newName = -Join($_.Name, " ", $index)
-		Write-Host "$($_.Name) will be changed to $newName" 
+		Write-Host "$($_.Name) will be renamed to $newName" 
 		$_.Editing.BeginEdit()
 		$_.Name = $newName
 		$_.Editing.EndEdit()
